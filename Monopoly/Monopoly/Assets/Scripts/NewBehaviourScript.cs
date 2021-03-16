ca6376ba69a15cf31f44a5e908dd9950b722e153
Austin Hunt <ahunt03087@gmail.com>
2021-03-15 21:25:22 -0400

    Update NewBehaviourScript.cs

--- Monopoly/Monopoly/Assets/Scripts/NewBehaviourScript.cs
+++ Monopoly/Monopoly/Assets/Scripts/NewBehaviourScript.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
 
-    Vector3[] boardPositions = {new Vector3(-15.5, 0.25, 16.5), new Vector3(-11.8, 0.25, 16.5), new Vector3(-9,0.25,16.5), //GO, MEDITERRANEAN, COMMUNITY
-                                new Vector3(-6, 0.25, 16.5), new Vector3(-3, 0.25, 16.5), new Vector3(0, 0.25, 16.5),  //BALTIC, INCOME, READING RAILROAD
-                                new Vector3(3, 0.25, 16.5), new Vector3(6, 0.25, 16.5), new Vector3(9, 0.25, 16.5), //ORIENTAL, CHANCE, VERMONT
-                                new Vector3(12, 0.25, 16.5), new Vector3(17.5, 0.25, 17.7), new Vector3(16.5, 0.25, 12), // CONNECTICUT, JAIL, ST CHARLES
-                                new Vector3(16.5, 0.25, 9), new Vector3(16.5, 0.25, 6), new Vector3(16.5, 0.25, 3), // ELECTRIC, STATES, VIRGINIA
-                                new Vector3(16.5, 0.25, 0), new Vector3(16.5, 0.25, -3), new Vector3(16.5, 0.25, -6), //PENNSYLVANIA, ST JAMES, COMMUNITY
-                                new Vector3(16.5, 0.25, -9), new Vector3(16.5, 0.25, -12), new Vector3(16.5, 0.25, -16.5), //TENESSEE, NEW YORK, FREE PARKING
-                                new Vector3(12, 0.25, -16.5), new Vector3(9, 0.25, -16.5), new Vector3(6, 0.25, -16.5), // KENTUCKY, CHANCE, INDIANA
-                                new Vector3(3, 0.25, -16.5), new Vector3(0, 0.25, -16.5), new Vector3(-3, 0.25, 16.5), //CHANCE, B&O RAILROAD, ATLANTIC
-                                new Vector3(-6, 0.25, -16.5), new Vector3(-9, 0.25, -16.5), new Vector3(-12, 0.25, -16.5), //VENTNOR, WATER WORKS, MARVIN GARDENS
-                                new Vector3(-16.5, 0.25, -16.5), new Vector3(-16.5, 0.25, -12), new Vector3(-16.5, 0.25, -9), //GOTO JAIL, PACIFIC, NORTH CAROLINA
-                                new Vector3(-16.5, 0.25, -6), new Vector3(-16.5, 0.25, -3), new Vector3(-16.5, 0.25, 0), //COMMUNITY, PENNSYLVANIA, SHORT LINE,
-                                new Vector3(-16.5, 0.25, 3), new Vector3(-16.5, 0.25, 6), new Vector3(-16.5, 0.25, 9), //CHANCE, PARK PLACE, LUXURY TAX
-                                new Vector3(-16.5, 0.25, 12), new Vector3(-16.5, 0.25, 16.5)}; //BOARDWALK, GO
+    //int POSITION_AMOUNT = 41;
+    Vector3[] boardPositions = {new Vector3((float)-16.5,(float) 0.25,(float) 16.5), new Vector3((float)-11.8,(float) 0.25,(float) 16.5), new Vector3((float)-9,(float)0.25,(float)16.5), //GO, MEDITERRANEAN, COMMUNITY
+                                new Vector3((float)-6,(float) 0.25, (float)16.5), new Vector3((float)-3,(float) 0.25,(float) 16.5), new Vector3((float)0,(float) 0.25,(float) 16.5),  //BALTIC, INCOME, READING RAILROAD
+                                new Vector3((float)3, (float)0.25,(float) 16.5), new Vector3((float)6,(float) 0.25,(float) 16.5), new Vector3((float)9,(float) 0.25,(float) 16.5), //ORIENTAL, CHANCE, VERMONT
+                                new Vector3((float)12,(float) 0.25,(float) 16.5), new Vector3((float)17.5,(float) 0.25, (float)17.7), new Vector3((float)16.5, (float)0.25, (float)12), // CONNECTICUT, JAIL, ST CHARLES
+                                new Vector3((float)16.5, (float)0.25, (float)9), new Vector3((float)16.5, (float)0.25, (float)6), new Vector3((float)16.5, (float)0.25, (float)3), // ELECTRIC, STATES, VIRGINIA
+                                new Vector3((float)16.5, (float)0.25, (float)0), new Vector3((float)16.5, (float)0.25, (float)-3), new Vector3((float)16.5, (float)0.25, (float)-6), //PENNSYLVANIA, ST JAMES, COMMUNITY
+                                new Vector3((float)16.5, (float)0.25, (float)-9), new Vector3((float)16.5, (float)0.25, (float)-12), new Vector3((float)16.5, (float)0.25, (float)-16.5), //TENESSEE, NEW YORK, FREE PARKING
+                                new Vector3((float)12, (float)0.25, (float)-16.5), new Vector3((float)9, (float)0.25, (float)-16.5), new Vector3((float)6, (float)0.25, (float)-16.5), // KENTUCKY, CHANCE, INDIANA
+                                new Vector3((float)3, (float)0.25, (float)-16.5), new Vector3((float)0, (float)0.25, (float)-16.5), new Vector3((float)-3, (float)0.25, (float)16.5), //CHANCE, B&O RAILROAD, ATLANTIC
+                                new Vector3((float)-6, (float)0.25, (float)-16.5), new Vector3((float)-9, (float)0.25, (float)-16.5), new Vector3((float)-12, (float)0.25, (float)-16.5), //VENTNOR, WATER WORKS, MARVIN GARDENS
+                                new Vector3((float)-16.5, (float)0.25, (float)-16.5), new Vector3((float)-16.5, (float)0.25, (float)-12), new Vector3((float)-16.5, (float)0.25, (float)-9), //GOTO JAIL, PACIFIC, NORTH CAROLINA
+                                new Vector3((float)-16.5,(float) 0.25, (float)-6), new Vector3((float)-16.5, (float)0.25, (float)-3), new Vector3((float)-16.5, (float)0.25,(float) 0), //COMMUNITY, PENNSYLVANIA, SHORT LINE,
+                                new Vector3((float)-16.5, (float)0.25, (float)3), new Vector3((float)-16.5, (float)0.25, (float)6), new Vector3((float)-16.5, (float)0.25, (float)9), //CHANCE, PARK PLACE, LUXURY TAX
+                                new Vector3((float)-16.5, (float)0.25, (float)12), new Vector3((float)-16.5, (float)0.25, (float)16.5)}; //BOARDWALK, GO
+    int currPos = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +32,24 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    int newPosition(int movementAmt)
+    {
+        int newPos = currPos + movementAmt;
+        if (newPos > boardPositions.Length) { //passes go
+            newPos = newPos - boardPositions.Length;
+            return newPos;
+        } else {
+            return newPos;
+        }
+    }
+
+    Vector3 getPositionVector(int position) {
+        if (position < boardPositions.Length){
+            return boardPositions[position];
+        } else {
+            return new Vector3(-1,-1,-1);
+        }
     }
 }
